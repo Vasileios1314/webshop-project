0a0f94a922dc05ccba68377598a449ca2f78bd9f
Vasileios1314 <vasilis.bobonis@hotmail.com>
2022-03-24 11:07:07 +0100

    new category router

--- routers/categories.js
+++ routers/categories.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const Categories = require("../models").category;
+const Products = require("../models").product;
 
 const router = new Router();
 
@@ -12,12 +13,13 @@ router.get("/", async (req, res) => {
   }
 });
 
-// wrong
 router.get("/:id", async (req, res) => {
   try {
     const categoriesById = req.params.id;
-    const categoryById = await Categories.findAll({
-      include: [{ model: Categories, where: { categoriesById }, right: true }],
+    const categoryById = await Products.findAll({
+      include: [
+        { model: Categories, where: { id: categoriesById }, right: true },
+      ],
     });
     if (!categoryById) {
       res.status(404).send("Product not found");
